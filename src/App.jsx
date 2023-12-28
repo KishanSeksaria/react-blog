ada803e6dff542e678040f07a1c696d60035d225
Kishan Seksaria <kishanseksaria@gmail.com>
2023-12-28 13:48:49 +0530

    add redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth.service';
+import { login, logout } from './store/authSlice';
 import './App.css';
-import conf from './conf/conf';
 
 function App() {
-  console.log(conf.appwriteUrl);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Blog app with appwrite!</h1>
-    </>
-  );
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then(userData => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? <div>Hello World!</div> : <div>Loading...</div>;
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Header from './headers/Header';
+import Footer from './footers/Footer';
+
+export { Header, Footer };
